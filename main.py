13e6a5227c700e101a0fd516cd84b02c35bc78f9
glogjones <138886032+glogjones@users.noreply.github.com>
2024-11-02 22:09:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,13 @@
 #
 
 import argparse
+
+import multiprocessing as mp
+
 import pprint
 import yaml
-import torch
 
+from src.utils.distributed import init_distributed
 from src.train import main as app_main
 
 parser = argparse.ArgumentParser()
@@ -19,31 +22,44 @@ parser.add_argument(
     default='configs.yaml')
 parser.add_argument(
     '--devices', type=str, nargs='+', default=['cuda:0'],
-    help='which device to use on local machine (default is cuda:0 for single GPU)')
+    help='which devices to use on local machine')
 
-def process_main(rank, fname, devices):
+
+def process_main(rank, fname, world_size, devices):
     import os
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(devices[0].split(':')[-1])  # Use the first device specified
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(devices[rank].split(':')[-1])
 
     import logging
     logging.basicConfig()
     logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+    if rank == 0:
+        logger.setLevel(logging.INFO)
+    else:
+        logger.setLevel(logging.ERROR)
 
     logger.info(f'called-params {fname}')
 
-    # Load script parameters
+    # -- load script params
+    params = None
     with open(fname, 'r') as y_file:
         params = yaml.load(y_file, Loader=yaml.FullLoader)
         logger.info('loaded params...')
         pp = pprint.PrettyPrinter(indent=4)
         pp.pprint(params)
 
-    logger.info(f'Running on device: {devices[0]}')
+    world_size, rank = init_distributed(rank_and_world_size=(rank, world_size))
+    logger.info(f'Running... (rank: {rank}/{world_size})')
     app_main(args=params)
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
 
-    # Run process_main directly for single-GPU execution
-    process_main(0, args.fname, args.devices)
+    num_gpus = len(args.devices)
+    mp.set_start_method('spawn')
+
+    for rank in range(num_gpus):
+        mp.Process(
+            target=process_main,
+            args=(rank, args.fname, num_gpus, args.devices)
+        ).start()
